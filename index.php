6ff8e3b8e24a60445c5abe89fbc48e605fb10a3f
Carl-Fredrik Herö <carlfredrik.hero@gmail.com>
2012-09-17 23:08:31 +0200

    Remove double commit message

--- index.php
+++ index.php
@@ -11,14 +11,7 @@ get('/signup', function($app){
 });
 
 post('/signup', function($app){
-	$user = new stdClass();
-	$user->type = 'user';
-	$user->name = $app->form('name');
-	$user->email = $app->form('email');
-	
-	echo json_encode($user);
-	
-	$app->set('message', 'Thanks for signing up' . $app->form('name') . '!');
+	$app->set('message', 'Thanks for signing up ' . $app->form('name') . '!');
 	$app->render('home');
 });
 
